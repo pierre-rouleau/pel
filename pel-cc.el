a39ffa7f831436d8b528004eb02d23a604e23c56
Pierre Rouleau <prouleau001@gmail.com>
2020-10-24 15:01:45 -0400

    pel-cc-mode-info show c-default-style and current CC mode style

--- pel-cc.el
+++ pel-cc.el
@@ -2,7 +2,7 @@
 
 ;; Created   : Friday, October 23 2020.
 ;; Author    : Pierre Rouleau <prouleau001@gmail.com>
-;; Time-stamp: <2020-10-23 17:11:16, updated by Pierre Rouleau>
+;; Time-stamp: <2020-10-23 18:50:57, updated by Pierre Rouleau>
 
 ;; This file is part of the PEL package.
 ;; This file is not part of GNU Emacs.
@@ -53,6 +53,26 @@
   (seq-filter 'pel-cc-filter-electric-key
           (mapcar 'string "#*/<>(){}:;,")))
 
+(defun pel-cc-c-default-style-for (mode)
+  "Return styles identified in variable `c-default-style' for MODE.
+Return a list of style strings in order of entry in the
+variable `c-default-style' if bound.  If it is unbound
+return \"void\"."
+  (if (boundp 'c-default-style)
+      (let ((styles '()))
+        (dolist (mode.style c-default-style (reverse styles))
+          (if (eq (car mode.style) mode)
+              (push (cdr mode.style) styles))))
+    "void"))
+
+(defun pel-cc-bracket-style-for (mode)
+  "Return the name of the PEL requested bracket style for MODE."
+  (let* ((mode-str (substring (symbol-name mode) 0 -5)) ; strip trailing "-mode"
+         (symbol-name (intern (format "pel-%s-bracket-style" mode-str))))
+    (if (boundp symbol-name)
+        (symbol-value symbol-name)
+      (format "unknown - %s is void" symbol-name))))
+
 ;;-pel-autoload
 (defun pel-cc-mode-info ()
   "Display information about current CC mode derivative."
@@ -60,25 +80,31 @@
   (let ((not-avail-msg "not available for this mode"))
     (message
      "%s state:
-- Indent width     : %s
-- Tab width        : %s
-- Indenting with   : %s
-- Bracket style    : %s
-- Comment style    : %s
-- Electric chars   : %s
-- Auto newline     : %s
-- Syntactic indent : %s
-- Hungry delete    : %s"
-     major-mode
-     (pel-symbol-on-off-string 'c-basic-offset)
-     tab-width
-     (pel-on-off-string indent-tabs-mode
-                        "hard-tabs and spaces"
-                        "spaces only")
-     (if (boundp 'c-default-style)
+- c-default-style     : %s
+- active style        : %s
+- Indent width        : %s
+- Tab width           : %s
+- Indenting with      : %s
+- c-indentation-style : %s
+- PEL Bracket style   : %s
+- Comment style       : %s
+- Electric chars      : %s
+- Auto newline        : %s
+- Syntactic indent    : %s
+- Hungry delete       : %s"
+     major-mode                                 ; 1
+     (pel-cc-c-default-style-for major-mode)    ; 2
+     (if (boundp 'c-default-style)              ; 3
          (alist-get major-mode c-default-style)
        "Unknown - c-default-style not loaded")
-     (if (and (boundp 'c-block-comment-flag)
+     (pel-symbol-on-off-string 'c-basic-offset) ; 4
+     tab-width                                  ; 5
+     (pel-on-off-string indent-tabs-mode        ; 6
+                        "hard-tabs and spaces"
+                        "spaces only")
+     (pel-symbol-value-or 'c-indentation-style) ; 7
+     (pel-cc-bracket-style-for major-mode)      ; 8
+     (if (and (boundp 'c-block-comment-flag)    ; 9
               (boundp 'c-block-comment-starter)
               (boundp 'c-block-comment-ender)
               (boundp 'c-block-comment-prefix))
@@ -91,16 +117,16 @@
            (format "Line comments: %s" (pel-symbol-value-or
                                         'c-line-comment-starter)))
        not-avail-msg)
-     (pel-symbol-on-off-string 'c-electric-flag
+     (pel-symbol-on-off-string 'c-electric-flag ; 10
                                (format "active: %s"
                                        (pel-concat-strings-in-list
                                         (pel-cc-electric-keys)))
                                "inactive"
                                not-avail-msg)
-     (pel-symbol-on-off-string 'c-auto-newline nil nil not-avail-msg)
-     (pel-symbol-on-off-string
+     (pel-symbol-on-off-string 'c-auto-newline nil nil not-avail-msg) ; 11
+     (pel-symbol-on-off-string                                        ; 12
       'c-syntactic-indentation nil nil not-avail-msg)
-     (pel-symbol-on-off-string 'c-hungry-delete-key
+     (pel-symbol-on-off-string 'c-hungry-delete-key                   ; 13
                                nil
                                "off, but the \
 F11-⌦  and F11-⌫  keys are available."
