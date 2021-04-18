fce1ce013b35c896a91b66cc8e0a2a7a77a8aad1
Pierre Rouleau <prouleau001@gmail.com>
2021-04-18 16:09:31 -0400

    Improved pel-cl-repl: now create a new REPL (with negative arg), selects existing REPL window if one is in the frame.

--- pel-commonlisp.el
+++ pel-commonlisp.el
@@ -33,7 +33,7 @@
 ;; Code hierarchy:
 ;;
 ;; * `pel-cl-repl'
-;;   - `pel-switch-to-buffer'
+;;   - `pel-switch-to-window'
 ;;     - `pel-select-buffer'
 ;;       - `pel-nth-elt'       (credit to Drew Adams for this one)
 ;;
@@ -74,10 +74,42 @@ Return buffer selected or nil if nothing selected."
         ;; 1 buffer in list
         (car buffer-candidates)))))
 
+(defun pel-switch-to-window (mode &optional in-other-window)
+  "Switch to window that has a buffer using specified MODE if any.
+MODE is a symbol.
+
+- If no buffer uses MODE return nil.
+- If one buffer uses MODE, use that buffer.
+- If several buffers use MODE, prompt user to identify which one
+  to select.
+
+If the buffer is currently displayed in a frame window, select
+that window. Otherwise open the buffer in the current window
+unless IN-OTHER-WINDOW is non-nil: then select another window to
+open the buffer using the MODE.
+
+Return the selected buffer using the MODE."
+(let* ((selected-buffer (pel-select-buffer mode)))
+  (when selected-buffer
+    (let ((window-to-use (get-buffer-window selected-buffer)))
+      (if window-to-use
+          (progn
+            (select-window window-to-use)
+            (switch-to-buffer selected-buffer))
+        (if in-other-window
+            (switch-to-buffer-other-window selected-buffer)
+          (switch-to-buffer selected-buffer)))))))
+
+;; TODO: move the following out into a more generic place
+;;       where the concept of creating REPL will be more general
+;;       and can be applied to more programming languages.
 (defun pel-switch-to-buffer (mode &optional in-other-window)
   "Switch to buffer in specified MODE if any.
 MODE is a symbol.
-Prompt user with a list with buffer names if there are several.
+
+Prompt user with a list with buffer names if there are several
+buffer using MODE.
+
 Return buffer selected, return nil if there are none or none selected.
 Use the other window if an IN-OTHER-WINDOW argument is specified."
   (let ((selected-buffer (pel-select-buffer mode)))
@@ -86,39 +118,60 @@ Use the other window if an IN-OTHER-WINDOW argument is specified."
           (switch-to-buffer-other-window selected-buffer)
         (switch-to-buffer selected-buffer)))))
 
-
-(defun pel-cl-repl (&optional in-other-window)
+(defun pel-cl-repl (&optional n)
   "Open or switch to Common-Lisp REPL buffer window.
 Use the Common Lisp REPL selected by the PEL user-options:
 - SLY when `pel-used-sly' is on and `pel-clisp-ide' is set to sly,
 - Slime when `pel-use-slime'is on and `pel-clisp-ide' is set to slime,
 - the inferior Lisp mode otherwise.
-Use the other window if an IN-OTHER-WINDOW argument is specified."
+
+The behaviour of the command is affected by the optional argument N:
+ - with no buffers running REPL:
+   > N is nil or absent:  - open REPL in current window
+   > N is positive:       - open REPL in other window
+   > N is negative:       - create new REPL in current window
+
+ - with 1 or more REPL already running:
+   - with 1 buffer already running it:
+     - use REPL buffer as target
+    - with multiple buffers already running it:
+      - prompt user to select the target REPL buffer
+        - if selected buffer is inside an opened window: switch to that window
+        - if selected buffer is not in an opened window:
+          > N is nil or absent:  - open REPL in current window
+          > N is positive:       - open REPL in other window
+          > N is negative:       - create new REPL in current window."
   (interactive "P")
-  (cond
-   ;;
-   ;; Use Slime
-   ((and pel-use-slime
-         (eq pel-clisp-ide 'slime))
-    (if (fboundp 'slime)
-        (unless (pel-switch-to-buffer 'slime-repl-mode in-other-window)
-          (slime))
-      (user-error "Function slime is unbound")))
-   ;;
-   ;; Use SLY
-   ((and pel-use-sly
-         (eq pel-clisp-ide 'sly))
-    (if (fboundp 'sly)
-        (unless (pel-switch-to-buffer 'sly-mrepl-mode in-other-window)
-          (sly))
-      (user-error "Function sly is unbound")))
-   ;;
-   ;; No IDE: use default Common Lisp REPL
-   (t
-    (if (fboundp 'run-lisp)
-        (unless (pel-switch-to-buffer 'inferior-lisp-mode in-other-window)
-          (run-lisp nil))
-      (user-error "Function run-lisp is unbound")))))
+  (let* ((n               (prefix-numeric-value n))
+         (in-other-window (and n (> n 0)))
+         (new-repl        (and n (< n 0))))
+    (cond
+     ;;
+     ;; Use Slime
+     ((and pel-use-slime
+           (eq pel-clisp-ide 'slime))
+      (if (fboundp 'slime)
+          (when (or new-repl
+                    (not (pel-switch-to-window 'slime-repl-mode in-other-window)))
+            (slime))
+        (user-error "Function slime is unbound")))
+     ;;
+     ;; Use SLY
+     ((and pel-use-sly
+           (eq pel-clisp-ide 'sly))
+      (if (fboundp 'sly)
+          (when (or new-repl
+                    (not (pel-switch-to-window 'sly-mrepl-mode in-other-window)))
+            (sly))
+        (user-error "Function sly is unbound")))
+     ;;
+     ;; No IDE: use default Common Lisp REPL
+     (t
+      (if (fboundp 'run-lisp)
+          (when (or new-repl
+                    (not (pel-switch-to-window 'inferior-lisp-mode in-other-window)))
+            (run-lisp nil))
+        (user-error "Function run-lisp is unbound"))))))
 
 ;;;---------------------------------------------------------------------------
 (provide 'pel-commonlisp)
