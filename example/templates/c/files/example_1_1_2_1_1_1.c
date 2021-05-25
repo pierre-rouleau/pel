15a3b236cc1d73215afc8b377e83c76176290c78
Pierre Rouleau <prouleau001@gmail.com>
2021-05-25 13:54:13 -0400

    Update C skeleton example content files.
    Only a sub-set of the files are now included.

--- example/templates/c/files/example_1_1_2_1_1_1.c
+++ example/templates/c/files/example_1_1_2_1_1_1.c
@@ -1,13 +1,30 @@
 /* C MODULE: example_1_1_2_1_1_1.c
 **
 ** Purpose   : Testing the template.
-** Created   : Sunday, May 23 2021.
+** Created   : Tuesday, May 25 2021.
 ** Author    : Pierre Rouleau <prouleau001@gmail.com>
-** Time-stamp: <2021-05-23 01:44:49, updated by Pierre Rouleau>
-** Copyright © 2021, Pierre Rouleau
-** License   : MIT
+** Time-stamp: <2021-05-25 12:34:33, updated by Pierre Rouleau>
+
+** Copyright (C) 2021  Pierre Rouleau
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+** THE SOFTWARE.
+
 */
-/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
 /* Module Description
 ** ------------------
@@ -20,7 +37,7 @@
 **   This file was generated with the following user-options values:
 **   - pel-c-skel-use-separators        : t
 **   - pel-c-skel-insert-file-timestamp : t
-**   - pel-c-skel-with-license          : "MIT"
+**   - pel-c-skel-with-license          : t
 **   - pel-c-skel-module-section-titles : ("Module Description" "Header Inclusion" "Local Types" "Local Variables" "Code")
 **   - pel-c-skel-doc-markup            : doxygen
 **   - pel-c-skel-comment-with-2stars   : t
