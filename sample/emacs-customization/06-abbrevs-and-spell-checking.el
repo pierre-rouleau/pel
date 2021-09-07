266a3868f18006225217610266b8dd5253be5611
Pierre Rouleau <prouleau001@gmail.com>
2021-09-07 07:51:43 -0400

    Add custom file sample for abbreviation and spell checking

---
+++ sample/emacs-customization/06-abbrevs-and-spell-checking.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 0)
+ '(column-number-mode t)
+ '(comment-empty-lines t)
+ '(confirm-nonexistent-file-or-buffer nil)
+ '(fill-column 78)
+ '(hl-line-sticky-flag nil)
+ '(imenu-max-items 50)
+ '(package-selected-packages (quote (bm popup which-key)))
+ '(pel-prompt-accept-y-n t)
+ '(pel-spell-check-personal-dictionary "~/.emacs.d/.ispell")
+ '(pel-spell-check-tool "aspell")
+ '(pel-use-bm t)
+ '(pel-use-hippie-expand t)
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
