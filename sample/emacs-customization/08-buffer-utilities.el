3b09fd7105e4d5859bb4cdd3b871c8f72ead2ed1
Pierre Rouleau <prouleau001@gmail.com>
2021-09-07 10:24:08 -0400

    Add custom file that add the buffer utilities

---
+++ sample/emacs-customization/08-buffer-utilities.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 0)
+ '(column-number-mode t)
+ '(comment-empty-lines t)
+ '(confirm-nonexistent-file-or-buffer nil)
+ '(fill-column 78)
+ '(hl-line-sticky-flag nil)
+ '(imenu-max-items 50)
+ '(package-selected-packages
+   (quote
+    (key-seq bind-key free-keys hydra bm popup which-key)))
+ '(pel-key-chords
+   (quote
+    ((global "" key-chord "<>" "<>")
+     (global "" key-chord "[]" "[]")
+     (c-mode "cc-mode" key-chord "{}" "{
+
+}")
+     (c++-mode "cc-mode" key-chord "{}" "{
+
+}")
+     (global "" key-chord "yu" pel-kc-windmove-up)
+     (global "" key-chord "bn" pel-kc-windmove-down)
+     (global "" key-chord "	q" pel-kc-indent-rigidly)
+     (flyspell-mode "flyspell" key-chord "4r" pel-kc-correct-word-before-point)
+     (flyspell-prog-mode "flyspell" key-chord "4r" pel-kc-correct-word-before-point)
+     (global "" key-chord "6y" pel-kc-open-at-point)
+     (global "" key-chord "6u" pel-kc-browse-filename-at-point)
+     (global "" key-chord "7u" pel-kc-browse-url-at-point)
+     (global "" key-chord ".;" pel-kc-search-word-from-top))))
+ '(pel-prompt-accept-y-n t)
+ '(pel-spell-check-personal-dictionary "~/.emacs.d/.ispell")
+ '(pel-spell-check-tool "aspell")
+ '(pel-use-ascii-table t)
+ '(pel-use-bind-key t)
+ '(pel-use-bm t)
+ '(pel-use-free-keys t)
+ '(pel-use-hippie-expand t)
+ '(pel-use-hydra t)
+ '(pel-use-iflipb t)
+ '(pel-use-key-chord (quote use-from-start))
+ '(pel-use-key-seq t)
+ '(pel-use-popup-switcher t)
+ '(pel-use-uniquify t)
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
