e282721c83561db5001fcd30dd08d80b0a627e4e
Pierre Rouleau <prouleau001@gmail.com>
2021-09-07 06:23:49 -0400

    Add samples of emacs-customization.el for very early customization.
    Could be used to quickly set things up.

---
+++ sample/emacs-customization/04-bookmark.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 0)
+ '(column-number-mode t)
+ '(comment-empty-lines t)
+ '(confirm-nonexistent-file-or-buffer nil)
+ '(fill-column 78)
+ '(hl-line-sticky-flag nil)
+ '(package-selected-packages (quote (bm popup which-key)))
+ '(pel-prompt-accept-y-n t)
+ '(pel-use-bm t)
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
