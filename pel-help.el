b6423686a7fa8dab1c3fd00ca68a05e6aa7d133f
Pierre Rouleau <prouleau001@gmail.com>
2024-01-05 18:34:38 -0500

    Fix docstring after (interactive)

--- pel-help.el
+++ pel-help.el
@@ -1,6 +1,6 @@
 ;;; pel-help.el --- PEL extra help utilities  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020, 2023  Pierre Rouleau
+;; Copyright (C) 2020, 2023, 2024  Pierre Rouleau
 
 ;; Author: Pierre Rouleau (concat "prouleau" "001" "@" "gmail" ".com")
 
@@ -41,17 +41,17 @@ Simple shortcut to invoke `describe-variable' on the `kill-ring' variable."
 
 ;;-pel-autoload
 (defun pel-show-major-mode ()
-  (interactive)
   "Display the symbol of the current major mode."
+  (interactive)
   (message "Major mode: %S" major-mode))
 
 
 ;;-pel-autoload
 (defun pel-show-function (&optional insert-it)
-  (interactive "P")
   "Display the name of the current function at point in mini-buffer.
 
 Also insert it at point when optional INSERT-IT argument is non-nil."
+  (interactive "P")
   (let ((fct-name (which-function)))
     (when (and fct-name insert-it)
       (insert fct-name))
