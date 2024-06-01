9161580b229977fd50111187ec25a9f3571b552b
Pierre Rouleau <prouleau001@gmail.com>
2024-05-31 23:43:46 -0400

    init.el exampke, includes nongnu alpa support in Emacs < 27

--- example/init/init.el
+++ example/init/init.el
@@ -264,6 +264,10 @@ Also expands to the file true name, replacing symlinks by what they point to."
                          (cons "melpa-stable"
                                (concat proto "://stable.melpa.org/packages/"))
                          t)
+            (add-to-list 'package-archives
+                         (cons "nongnu"
+                               (concat proto "://elpa.nongnu.org/nongnu/"))
+                         t)
             (when (< emacs-major-version 24)
               ;; For important compatibility libraries like cl-lib
               (add-to-list 'package-archives
