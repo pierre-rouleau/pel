213b8af9370c87ab25aba195f99a674a2c1514e8
Pierre Rouleau <prouleau001@gmail.com>
2024-12-20 17:45:09 -0500

    Use better percritic output format to allow navigation

--- pel-perl.el
+++ pel-perl.el
@@ -2,7 +2,7 @@
 
 ;; Created   : Friday, December 20 2024.
 ;; Author    : Pierre Rouleau <prouleau001@gmail.com>
-;; Time-stamp: <2024-12-20 16:40:55 EST, updated by Pierre Rouleau>
+;; Time-stamp: <2024-12-20 17:44:35 EST, updated by Pierre Rouleau>
 
 ;; This file is part of the PEL package.
 ;; This file is not part of GNU Emacs.
@@ -43,7 +43,8 @@ Show errors in compilation-mode buffer."
   (interactive)
   (if (executable-find "perlcritic")
       (compile
-       (format "perlcritic %s"
+       ;; use a format that can be used by the compile mode to move to the error.
+       (format "perlcritic --nocolor --verbose \"%%F:%%l:%%c:\\tSev:%%s:\\t%%m.\\t(%%e)\\n\" %s"
                (shell-quote-argument (buffer-file-name)))
        nil)
       (user-error "Please install perlcritic")))
