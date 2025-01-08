cef48295c41c22da3713fea036651c4de09242ba
Pierre Rouleau <prouleau001@gmail.com>
2025-01-07 22:54:20 -0500

    Add <f12> ? binding in Perl buffer to get help

--- pel_keys.el
+++ pel_keys.el
@@ -4670,6 +4670,7 @@ See lsp-keymap-prefix and pel-activate-f9-for-greek user-options."))
   (define-key pel:for-perl (kbd "<down>")   'end-of-defun)
   (define-key pel:for-perl "c"              'pel-perl-critic)
   (when pel-perl-mode
+    (define-key pel:for-perl "?"            'cperl-perldoc-at-point)
     (define-key pel:for-perl "|"            'cperl-lineup))
 
   ;; the perl-mode is part of Emacs
