6dd32b6b9b2476a29e1ede0c0e399720c5b06a4e
Pierre Rouleau <prouleau001@gmail.com>
2025-08-27 16:00:31 -0400

    Improved pel-show-fill-columns : more info & more control.

--- pel-fill.el
+++ pel-fill.el
@@ -1,6 +1,6 @@
 ;;; pel-fill.el --- PEL Source code management -*-lexical-binding: t-*-
 
-;; Copyright (C) 2020, 2021, 2022  Pierre Rouleau
+;; Copyright (C) 2020, 2021, 2022, 2025  Pierre Rouleau
 
 ;; Author: Pierre Rouleau <prouleau001@gmail.com>
 
@@ -41,26 +41,46 @@ Activate/de-activate automatic filling in source code comments only."
   (pel-toggle 'comment-auto-fill-only-comments))
 
 
-;;-pel-autoload
-(defun pel-show-fill-columns ()
-  "Display value of relevant fill columns for current buffer."
-  (interactive)
-  (message "auto-fill-mode: %s\n%s"
-           (if auto-fill-function "on" "off")
-           (if (memq (pel-major-mode-of)
-                     '(emacs-lisp-mode lisp-interaction-mode lisp-mode))
-               (format "Lisp fill columns:
-- comment  : %S
-- docstring: %S
-- code     : %S"
-                       (or comment-fill-column fill-column)
-                       emacs-lisp-docstring-fill-column
-                       (current-fill-column))
-             (format "Fill column:
-- comment  : %S
-- text     : %S"
-                     (or comment-fill-column fill-column)
-                     (current-fill-column)))))
+(defun pel-show-fill-columns (&optional append)
+  "Display value of relevant fill columns for current buffer.
+
+Clear previous buffer content unless optional APPEND argument is non-nil,
+in which case it appends to the previous report."
+  (interactive "P")
+  (let* ((fill-column-symbol (pel-major-mode-symbol-for "pel-%s-fill-column"))
+        (fill-column-symbol-is-bound (boundp fill-column-symbol))
+        (text-fill-column (current-fill-column))
+        (isa-lisp-mode (memq (pel-major-mode-of)
+                             '(emacs-lisp-mode
+                               lisp-interaction-mode
+                               lisp-mode)))
+        (pel-insert-symbol-content-context-buffer (current-buffer)))
+    (pel-print-in-buffer
+     "*pel-fill-info*"
+     "Fill Control"
+     (lambda ()
+       "Print fill control variable values."
+       (pel-insert-symbol-content-line 'auto-fill-function)
+       (pel-insert-symbol-content-line 'auto-fill-inhibit-regexp)
+       (pel-insert-symbol-content-line 'fill-column)
+       (insert (format ", active value for text: %S" text-fill-column))
+       (when fill-column-symbol-is-bound
+         (pel-insert-symbol-content-line fill-column-symbol)
+         (insert ", under PEL this controls fill-column."))
+       (when isa-lisp-mode
+         (pel-insert-symbol-content-line 'emacs-lisp-docstring-fill-column))
+       (pel-insert-symbol-content-line 'default-justification)
+       (pel-insert-symbol-content-line 'fill-prefix)
+       (pel-insert-symbol-content-line 'left-margin)
+       (pel-insert-symbol-content-line 'fill-individual-varying-indent)
+       (pel-insert-symbol-content-line 'sentence-end-double-space)
+       (pel-insert-symbol-content-line 'sentence-end-without-period)
+       (pel-insert-symbol-content-line 'comment-fill-column)
+       (pel-insert-symbol-content-line 'display-fill-column-indicator)
+       (pel-insert-list-content  'global-display-fill-column-indicator-modes))
+     (unless append :clear-buffer)
+     :use-help-mode)))
+
 
 ;; -----------------------------------------------------------------------------
 (provide 'pel-fill)
