50534e0e2f2e13f4f3c6f143eff957858e426c05
Pierre Rouleau <prouleau001@gmail.com>
2026-03-21 19:36:54 -0400

    Fix skipped test

--- test/pel-erlang-test.el
+++ test/pel-erlang-test.el
@@ -2,7 +2,7 @@
 
 ;; Created   : Friday, March 21 2026.
 ;; Author    : Pierre Rouleau <prouleau001@gmail.com>
-;; Time-stamp: <2026-03-21 19:04:38 EDT, updated by Pierre Rouleau>
+;; Time-stamp: <2026-03-21 19:36:24 EDT, updated by Pierre Rouleau>
 
 ;; This file is part of the PEL package.
 ;; This file is not part of GNU Emacs.
@@ -62,15 +62,14 @@
   ;;
   ;; We test for substrings so the tests remain valid even if cosmetic wording
   ;; in the messages changes.
-  '((t                 "erlang-mode")
-    (with-tree-sitter  "erlang-ts-mode"))
+  '((t                 "use erlang-mode")
+    (with-tree-sitter  "use erlang-ts-mode"))
   "Scenarios for `ert-test-pel-erlang-mode-used-text'.")
 
 (ert-deftest ert-test-pel-erlang-mode-used-text ()
   "Test `pel-erlang-mode-used-text'."
   ;; Known valid values.
-  (ert-skip "Temporary skip test under development.")
-  (should (pel-string= (pel-erlang-mode-used-text t)
+(should (pel-string= (pel-erlang-mode-used-text t)
                        "use erlang-mode from erlang-mode.el."
                        't))
   (should (pel-string= (pel-erlang-mode-used-text 'with-tree-sitter)
