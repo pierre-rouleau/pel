73ffda7a278da6f67208e9d731aeca2b5018cc6f
Pierre Rouleau <prouleau001@gmail.com>
2026-04-13 23:05:14 -0400

    Cleanup Gleam support.

--- pel-gleam.el
+++ pel-gleam.el
@@ -2,7 +2,7 @@
 
 ;; Created   : Monday, October  6 2025.
 ;; Author    : Pierre Rouleau <prouleau001@gmail.com>
-;; Time-stamp: <2026-03-26 14:43:23 EDT, updated by Pierre Rouleau>
+;; Time-stamp: <2026-04-13 22:51:51 EDT, updated by Pierre Rouleau>
 
 ;; This file is part of the PEL package.
 ;; This file is not part of GNU Emacs.
@@ -25,7 +25,30 @@
 ;;; --------------------------------------------------------------------------
 ;;; Commentary:
 ;;
+;; Support for the Gleam programming language.
 ;;
+;; Glean support is now exclusively provided by a Tree-Sitter based major
+;; mode.  Their old code was originally implemented using a classic Emacs
+;; major mode but the Gleam team dropped support for that code and now only
+;; support a Tree-Sitter based implementation.
+;;
+;; PEL therefore only supports a Gleam on Emacs 28 to get a stable Tree-Sitter
+;; implementation and does not not support turning Tree-Sitter off for Gleam.
+;;
+;;
+;; The provided function are:
+;;
+;; Reformatting code on buffer save
+;;  * `pel-gleam-toggle-format-on-buffer-save'
+;;
+;;  Indentation Control Information
+;;  * `pel-gleam-indent-tab-info'
+;;    - `pel-gleam-insert-indent-info'
+;;    - `pel-gleam-insert-tab-info'
+;;
+;;  Major Mode Setup Information
+;;  * `pel-gleam-setup-info'
+;;    - `pel--gleam-minor-mode-info'
 
 ;;; --------------------------------------------------------------------------
 ;;; Dependencies:
@@ -47,6 +70,9 @@
 ;;; Code:
 ;;
 
+;;* Reformatting code on buffer save
+;;  ================================
+
 ;;-pel-autoload
 (defun pel-gleam-toggle-format-on-buffer-save (&optional globally)
   "Toggle automatic execution of gleam format when saving Gleam files.
@@ -59,19 +85,43 @@ customized value of the `gleam-ts-format-on-save' user option."
   (interactive "P")
   (pel-toggle-and-show-user-option 'gleam-ts-format-on-save globally))
 
+;; ---------------------------------------------------------------------------
+;;* Indentation Control Information
+;;  ===============================
+;;
+;; Define a set of major-mode specific call back functions that are called
+;; by the `pel-show-indent' command.  The call backs are invoked indirectly.
+;; The `pel-indent-control-context' infer the names of the call back functions
+;; using the name of the major mode and a format string.
+;;
+;; The call back functions are:
+;; - `pel-gleam-insert-indent-info' : Insert non-inferred indentation control.
+;; - `pel-gleam-insert-tab-info'    : Insert tab control info.
+;; - `pel-gleam-indent-tab-info'    : Tab information command.
+
+;; Define the function called by `pel-indent-control-context' used by the
+;; command `pel-show-indent' to insert the indentation control user option.
+
 ;;-pel-autoload
 (defun pel-gleam-insert-indent-info ()
   "Insert Gleam indentation and hard tab setup info in current context.
-Return a list of generic symbols described."
-  (pel-insert-symbol-content-line 'gleam-ts-indent-offset)
-  ;; Return the list of generic symbols described here.
+Return a list of generic symbols described by this function."
+  ;; `pel-indent-insert-control-info' identifies the gleam indentation control
+  ;; variable via a call to `pel-mode-indent-control-vars' which uses
+  ;; `pel-mode-or-ts-mode-indent-control-vars' to identify
+  ;; `gleam-ts-indent-offset'.  Therefore no user-option value/link is
+  ;; inserted here, just a note that only Tree-Sitter mode variable is used.
+  (insert "\nGleam has no classic mode.")
+  ;; Return the list of generic symbols this function inserts.
+  ;; These symbols are later used to identify the information already
+  ;; inserted.
   '(indent-description-info))
 
 
 ;;-pel-autoload
 (defun pel-gleam-insert-tab-info ()
   "Insert Gleam indentation and hard tab setup info in current context.
-Return a list of generic symbols described."
+Return a list of generic symbols described by this function."
   (insert (substitute-command-keys "
  The Gleam designers are pushing for an 2-column indentation
  made of space only. No tabs.
@@ -86,6 +136,11 @@ Return a list of generic symbols described."
 
  You can disable the format on save while you work on that file.
  When you're done just untabify the code with \\[untabify].
+
+ PEL can automate all that: simply activate the
+ `pel-indent-with-tabs-mode-for-gleam'  user-option below.
+ This does the same as the tbindent-mode implemented
+ as a separate package.
 "))
   (pel-insert-symbol-content-line 'pel-gleam-tab-width)
   (pel-insert-symbol-content-line 'pel-gleam-use-tabs
@@ -118,6 +173,9 @@ Return a list of generic symbols described."
      (unless append :clear-buffer)
      :use-help-mode)))
 
+;; ---------------------------------------------------------------------------
+;;* Major Mode Setup Information
+;; =============================
 
 (defun pel--gleam-minor-mode-info ()
   "Insert information related to Gleam minor modes."
@@ -143,7 +201,7 @@ following user-options:")
      "PEL setup for Gleam programming language"
      (lambda ()
        "Print Gleam setup"
-       (insert (propertize "* Major Mode Control:" 'face 'bold))
+       (pel-insert-bold "* Major Mode Control:")
        (insert "
 - Note: Gleam is currently only supported by a Tree-Sitter aware mode.")
        (pel-insert-symbol-content 'major-mode nil :on-same-line nil
@@ -153,7 +211,7 @@ following user-options:")
                                    'gleam "\n- "))))
        (insert "\n\n")
        ;;
-       (insert (propertize "* Code Formatting Control:" 'face 'bold))
+       (pel-insert-bold "* Code Formatting Control:")
        (pel-insert-symbol-content-line 'gleam-ts-format-on-save
                                        nil
                                        (lambda (v)
