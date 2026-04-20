fea79742f2783e212e7ab8bad155e1514a2144f0
Pierre Rouleau <prouleau001@gmail.com>
2026-04-20 12:04:41 -0400

    Add test files

---
+++ test/test-files/objective-c.h
@@ -0,0 +1,32 @@
+// OBJC FILE: objective-c.h
+//
+// Purpose   : Test PEL's ability to detect Objective-C .h files.
+// Created   : Thursday, April  9 2026.
+// Author    : Pierre Rouleau <prouleau001@gmail.com>
+// Time-stamp: <2026-04-09 15:38:51 EDT, updated by Pierre Rouleau>
+// -----------------------------------------------------------------------------
+// Module Description
+// ------------------
+//
+// The following are C++ keywords.
+// They re not detected because they are inside a comment.
+//
+// class
+// private:
+
+
+// -----------------------------------------------------------------------------
+// Dependencies
+// ------------
+//
+//
+
+
+// -----------------------------------------------------------------------------
+// Code
+// ----
+//
+//
+
+
+// -----------------------------------------------------------------------------
